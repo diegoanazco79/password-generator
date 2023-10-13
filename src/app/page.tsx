b1d7cebefb182e83a472002f1f838981d610bbed
Diego Añazco <danazco@vozy.co>
2023-10-12 21:44:32 -0500

    :lipstick: COSMETIC :: Change green color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,12 +23,12 @@ export default function Home () {
       <h1 className='text-gray-300 text-2xl'>Password Generator</h1>
       <div className='flex items-center justify-between p-5 bg-[#24232b] max-w-screen-sm w-full mt-6'>
         <span className='text-gray-500 text-2xl'> P4$5W0rD!</span>
-        <FaRegCopy className='w-[25px] h-[25px] text-[#a6efad] cursor-pointer hover:text-[#83be89]' />
+        <FaRegCopy className='w-[25px] h-[25px] text-[#00ff00] cursor-pointer hover:text-[#00ff00ba]' />
       </div>
       <div className='p-5 bg-[#24232b] max-w-screen-sm w-full mt-6'>
         <div className='flex items-center justify-between'>
           <span className='text-gray-100 text-xl'>Character Length</span>
-          <span className='text-3xl text-[#a6efad]'>{passwordLength}</span>
+          <span className='text-3xl text-[#00ff00]'>{passwordLength}</span>
         </div>
         <InputRange
           value={passwordLength}
@@ -46,15 +46,15 @@ export default function Home () {
         <div className='bg-[#0f0e14] p-4 mt-6 flex justify-between'>
           <span className='text-gray-500 text-lg'>STRENGTH</span>
           <div className='flex'>
-            <div className={`border-2 border-gray-100 px-1 mr-2 ${passwordStrength >= 1 ? 'bg-[#f8ce61] border-[#f8ce61]' : ''} `} />
-            <div className={`border-2 border-gray-100 px-1 mr-2 ${passwordStrength >= 2 ? 'bg-[#f8ce61] border-[#f8ce61]' : ''} `} />
-            <div className={`border-2 border-gray-100 px-1 mr-2 ${passwordStrength >= 3 ? 'bg-[#f8ce61] border-[#f8ce61]' : ''} `} />
-            <div className={`border-2 border-gray-100 px-1 mr-2 ${passwordStrength >= 4 ? 'bg-[#f8ce61] border-[#f8ce61]' : ''} `} />
-            <div className={`border-2 border-gray-100 px-1 mr-2 ${passwordStrength >= 5 ? 'bg-[#f8ce61] border-[#f8ce61]' : ''} `} />
+            <div className={`border-2 px-1 mr-2 ${passwordStrength >= 1 ? 'bg-[#f8ce61] border-[#f8ce61]' : 'border-gray-100'} `} />
+            <div className={`border-2 px-1 mr-2 ${passwordStrength >= 2 ? 'bg-[#f8ce61] border-[#f8ce61]' : 'border-gray-100'} `} />
+            <div className={`border-2 px-1 mr-2 ${passwordStrength >= 3 ? 'bg-[#f8ce61] border-[#f8ce61]' : 'border-gray-100'} `} />
+            <div className={`border-2 px-1 mr-2 ${passwordStrength >= 4 ? 'bg-[#f8ce61] border-[#f8ce61]' : 'border-gray-100'} `} />
+            <div className={`border-2 px-1 mr-2 ${passwordStrength >= 5 ? 'bg-[#f8ce61] border-[#f8ce61]' : 'border-gray-100'} `} />
           </div>
         </div>
 
-        <button className='mt-6 bg-[#a6efad] w-full px-6 py-4 font-semibold text-xl hover:bg-transparent hover:border-[#a6efad] hover:border border border-transparent hover:text-[#a6efad]'>
+        <button className='mt-6 bg-[#00ff00] transition ease-in-out duration-300 w-full px-6 py-4 font-semibold text-2xl hover:bg-transparent hover:border-[#00ff00] hover:border border border-transparent hover:text-[#00ff00]'>
           GENERATE
         </button>
       </div>
